e9a8fc08e2031c88fb74fa5c10e30e5c10ec1605
xyz <q9776887181@gmail.com>
2021-07-15 13:15:41 +0800

    update

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'scrapy-custom-tool',
+    version = '0.0.1',
+    keywords = (),
+    description = '',
+    long_description = '',
+    license = '',
+    install_requires = [
+
+    ],
+    author = 'aq',
+    author_email = 'q9776887181@gmail.com',
+    packages = find_packages(),
+    platforms = 'any',
+    url = '',
+    include_package_data = True,
+    entry_points = {
+        'console_scripts': [
+            
+        ]
+    }
+)
+
